d11bfc1e20a61c8a4e9180b1f99b7627744cea47
Dom1n1cIR <d.robeson0511@gmail.com>
2021-03-04 17:20:40 +0000

    tidying up front-end and setting up initial dashboard & login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
+import { Container } from "reactstrap";
+import Routes from './routes';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      learning the mern stack
-    </div>
+    <Container>
+    <h1>Sport's App</h1>
+    <Routes />
+    </Container>
   );
 }
 
