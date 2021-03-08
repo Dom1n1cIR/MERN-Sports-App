3bf4bf3b77a3b080e7f3686d7ac29c04f2a2f60c
Dom1n1cIR <d.robeson0511@gmail.com>
2021-03-08 20:59:06 +0000

    adding styling and basic validation to events/registration and login page

--- frontend/src/pages/Login/index.js
+++ frontend/src/pages/Login/index.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import api from '../../Services/api'
-import { Container, Button, Form, FormGroup, Input } from 'reactstrap';
+import { Container, Button, Form, FormGroup, Input, Alert } from 'reactstrap';
 
 export default function Login({ history }) {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
-
+    const [error, setError] = useState(false)
+    const [errorMessage, setErrorMessage] = useState("false")
 
     const handleSubmit = async evt => {
         evt.preventDefault();
@@ -14,20 +15,32 @@ export default function Login({ history }) {
         const response = await api.post('/login', { email, password })
         const userId = response.data._id || false;
 
-        if (userId) {
-            localStorage.setItem('user', userId)
-            history.push('/dashboard')
-        } else {
-            const { message } = response.data
-            console.log(message)
+        try {
+
+            if (userId) {
+                localStorage.setItem('user', userId)
+                history.push('/dashboard')
+            } else {
+                const { message } = response.data
+                setError(true)
+                setErrorMessage(message)
+                setTimeout(() => {
+                    setError(false)
+                    setErrorMessage(null)
+                }, 2000)
+            }
+            
+        } catch (error) {
+            
         }
+
     }
 
     return (
         <Container>
             <h2>Login:</h2>
             <p>Please <strong>Login</strong> into your account</p>
-            <Form onSubmit={handleSubmit}>
+            <Form onSubmit={handleSubmit} className="content">
                 <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
                     <Input type="email" name="email" id="email" placeholder="Your email" onChange={evt => setEmail(evt.target.value)} />
                 </FormGroup>
@@ -36,6 +49,9 @@ export default function Login({ history }) {
                 </FormGroup>
                 <Button>Submit</Button>
             </Form>
+            {error ? (
+                <Alert className="event-validation" color="danger"> Missing required information</Alert>
+            ) : null}
         </Container>
     );
 }
